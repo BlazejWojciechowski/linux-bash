1009b72eb1cbaee4b309fefed0c3dbcf5167ac28
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-02 11:27:01 +0200

    initial commit

---
+++ src/test/java/blaze/linux/bash/BashApplicationTests.java
@@ -0,0 +1,13 @@
+package blaze.linux.bash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
